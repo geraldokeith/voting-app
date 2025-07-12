e3a894eafa8c574c332d31aed3795fcfbb294947
geraldokeith <geraldo2keith@gmail.com>
2025-07-12 19:24:32 +0300

    Fixed deployment config

--- pollagent/settings.py
+++ pollagent/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'pollagent.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
+"""
 
 if os.environ.get('RAILWAY_ENVIRONMENT'):
     DATABASES = {
@@ -91,12 +91,13 @@ if os.environ.get('RAILWAY_ENVIRONMENT'):
         }
     }
 else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    }
+"""
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
